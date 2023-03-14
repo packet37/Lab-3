e535d95491aef6b8f97f6dd312c8e64b162f1e95
packet37 <125910700+packet37@users.noreply.github.com>
2023-03-14 15:14:10 +0300

    Add files via upload

---
+++ Лабораторная_3.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector v1 = new Vector(new double[] { 1, 2, 3 });
+        Vector v2 = new Vector(new double[] { 4, 5, 6 });
+
+        Console.WriteLine("v1: " + v1.ToString());
+        Console.WriteLine("v2: " + v2.ToString());
+
+        Vector v3 = Vector.Add(v1, v2);
+        Console.WriteLine("v1 + v2 = " + v3.ToString());
+
+        double dotProduct = Vector.DotProduct(v1, v2);
+        Console.WriteLine("v1 * v2 = " + dotProduct);
+
+        bool equal = Vector.AreEqual(v1, v2);
+        Console.WriteLine("v1 == v2: " + equal);
+
+        Vector v4 = new Vector(3);
+        v4.ReadFromConsole();
+        Console.WriteLine("v4: " + v4.ToString());
+        Console.WriteLine("Модуль v4: " + v4.GetMagnitude());
+        Console.WriteLine("Максимальный элемент v4: " + v4.GetMaxElement());
+        Console.WriteLine("Индекс минимального элемента v4: " + v4.GetMinElementIndex());
+        //Методы из индвидуального задания
+        Console.WriteLine("Введите индекс элемента, который хотите увидеть");
+        int newindex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение элемента, который хотите увидеть");
+        int newvalue = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Элемент с введенным индексом: " +v4.ElementByIndex(newindex));
+        Console.WriteLine("Измененный вектор" + v4.ChangingElement(newindex,newvalue));
+        Console.WriteLine("Найденный элемент" + v4.LinearSearch());
+        Console.WriteLine("Упорядочен ли массив по возрастанию: " + v4.IsSorted());
+        Console.WriteLine("Введите, на сколько позиций вы хотите сдвинуть вектор: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Вектор, сдвинутый на n позиций вправо" + v4.Right(n));
+        Vector v5 = v4.GetPositiveElementsVector();
+        Console.WriteLine("v4: " + v5.ToString());
+
+        Console.ReadKey();
+    }
+}
---
+++ Лабораторная_3_Класс.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+class Vector
+{
+    private double[] elements;
+
+    // Конструкторы
+    public Vector(int size)
+    {
+        elements = new double[size];
+    }
+
+    public Vector(double[] values)
+    {
+        this.elements = new double[values.Length];
+        elements.CopyTo(this.elements, 0);
+    }
+
+    public Vector(Vector vector)
+    {
+        elements = vector.elements;
+    }
+
+    //Свойство для доступа к элементам массива
+    public double this[int index]
+    {
+        get { return elements[index]; }
+        set { elements[index] = value; }
+    }
+
+    //Метод ввода с консоли
+    public void ReadFromConsole()
+    {
+        for (int i = 0; i < elements.Length; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            elements[i] = double.Parse(Console.ReadLine());
+        }
+    }
+
+    //Метод для вывода вектора в виде строки
+    public override string ToString()
+    {
+        return string.Join(", ", elements);
+    }
+
+    //Метод для вычисления модуля вектора
+    public double GetMagnitude()
+    {
+        return Math.Sqrt(elements.Select(e => e * e).Sum());
+    }
+
+    //Метод для получения наибольшего элемента вектора
+    public double GetMaxElement()
+    {
+        return elements.Max();
+    }
+
+    //Метод для получения индекса наименьшего элемента вектора
+    public int GetMinElementIndex()
+    {
+        return Array.IndexOf(elements, elements.Min());
+    }
+
+    //Метод для создания нового вектора, состоящего только из положительных элементов
+    public Vector GetPositiveElementsVector()
+    {
+        return new Vector(elements.Where(e => e > 0).ToArray());
+    }
+
+    //Метод для сложения двух векторов
+    public static Vector Add(Vector v1, Vector v2)
+    {
+        if (v1.elements.Length != v2.elements.Length)
+        {
+            throw new ArgumentException("Векторы должны быть одинаковой длины");
+        }
+
+        double[] result = new double[v1.elements.Length];
+        for (int i = 0; i < v1.elements.Length; i++)
+        {
+            result[i] = v1.elements[i] + v2.elements[i];
+        }
+
+        return new Vector(result);
+    }
+
+    //Метод для вычисления скалярного произведения двух векторов
+    public static double DotProduct(Vector v1, Vector v2)
+    {
+        if (v1.elements.Length != v2.elements.Length)
+        {
+            throw new ArgumentException("\"Векторы должны быть одинаковой длины");
+        }
+
+        double result = 0;
+        for (int i = 0; i < v1.elements.Length; i++)
+        {
+            result += v1.elements[i] * v2.elements[i];
+        }
+
+        return result;
+    }
+
+    //Метод для проверки двух векторов на равенство
+    public static bool AreEqual(Vector v1, Vector v2)
+    {
+        if (v1.elements.Length != v2.elements.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < v1.elements.Length; i++)
+        {
+            if (v1.elements[i] != v2.elements[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    //Метод для возврата одного элемента по индексу
+    public  double ElementByIndex(int newindex)
+    {
+        return elements[newindex];
+    }
+    //Метод для изменения одного элемента вектора по индексу и новому значению элемента
+    public  double ChangingElement(int newindex, double newvalue)
+    {
+        return elements[newindex] = newvalue;
+    }
+    //Метод для линейного поиска элемента в массиве
+    public int LinearSearch(double value)
+    {
+        for (int i = 0; i < v4.elements.Length; i++)
+        {
+            if (elements[i] == value)
+                return i;
+        }
+        return 1;
+    }
+    //Метод для проверки того, что массив упорядочен по возрастанию 
+    public bool IsSorted()
+    {
+        for (int i = 1; i < v4.elements.Length; i++)
+        {
+            if (elements[i] < elements[i - 1])
+                return false;
+        }
+        return true;
+    }
+
+    //Вариант 7 
+    //Метод для сдвига элементов вектора на заданное число позиций вправо, освободившиеся элементы заполняются нулями
+    public void Right(int n)
+    {
+        for (int i = 0; i < v4.elements.Length - n; i++)
+        {
+            this.elements[v4.elements.Length - 1 - i] = this.elements[v4.elements.Length - 1 - i - n];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            this.elements[i] = 0;
+        }
+    }
+}
